6bb85525e0170ce6ca27e896a86d09f4ec0c08b3
sifter0926 <sifter0926@gmail.com>
2024-10-25 11:11:20 +0900

    fileuploadPro controller

--- src/main/java/com/pgm/jpademo2/controller/UpDownController.java
+++ src/main/java/com/pgm/jpademo2/controller/UpDownController.java
@@ -2,13 +2,24 @@ package com.pgm.jpademo2.controller;
 
 import com.pgm.jpademo2.dto.BoardDTO;
 import com.pgm.jpademo2.dto.upload.UploadFileDTO;
+import com.pgm.jpademo2.dto.upload.UploadResultDTO;
 import lombok.extern.log4j.Log4j2;
+import net.coobird.thumbnailator.Thumbnailator;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 @Controller
 @Log4j2
@@ -26,9 +37,34 @@ public class UpDownController {
         //log.info(uploadFileDTO);
         log.info(boardDTO);
         if(uploadFileDTO.getFiles() !=null){
-            uploadFileDTO.getFiles().forEach(file -> {
-                String originalName = file.getOriginalFilename();
+            final List<UploadResultDTO> list = new ArrayList<>();
+            uploadFileDTO.getFiles().forEach(multipartFile -> {
+                String originalName = multipartFile.getOriginalFilename();
                 log.info(originalName);
+
+                String uuid = UUID.randomUUID().toString();
+                Path savePath = Paths.get(uploadPath, uuid+"_"+ originalName);
+                boolean image = false;
+                try {
+                    multipartFile.transferTo(savePath); // 서버에 파일저장
+                    //이미지 파일의 종류라면
+                    if(Files.probeContentType(savePath).startsWith("image")){
+                        image = true;
+                        File thumbFile = new File(uploadPath, "s_" + uuid+"_"+ originalName);
+                        Thumbnailator.createThumbnail(savePath.toFile(), thumbFile, 100,100);
+                    }
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                list.add(UploadResultDTO.builder()
+                        .uuid(uuid)
+                        .fileName(originalName)
+                        .img(image)
+                        .build()
+                );
+                model.addAttribute("list", list);
+                model.addAttribute("uploadPath", uploadPath);
             });
         }
 
